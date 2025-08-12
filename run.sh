5e85a7b1530007e751e13a8366b7d38dcf04831c
yangro <dldidfh2@gmail.com>
2025-08-12 10:20:57 +0900

    fix: onnx export add normalize

--- run.sh
+++ run.sh
@@ -2,8 +2,7 @@ python export.py
 
 trtexec \
 --onnx=onnx_export/PE-Core-L14-336/PE-Core-L14-336_vision.onnx \
---saveEngine=vision.engine \
+--saveEngine=trt_export/PE-Core-L14-336/PE-Core-L14-336_vision.engine \
 --minShapes=image:1x3x336x336 \
 --optShapes=image:4x3x336x336 \
---maxShapes=image:16x3x336x336 \
---verbose 
+--maxShapes=image:16x3x336x336
